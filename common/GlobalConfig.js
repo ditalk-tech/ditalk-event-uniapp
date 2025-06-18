ead144e8d8369138366540c1fb35bdf27d47c0d0
weidixian <weidixian@126.com>
2025-06-18 20:53:44 +0800

    update 更新小程序的platform为weixin

--- common/GlobalConfig.js
+++ common/GlobalConfig.js
@@ -1,7 +1,7 @@
 export const tenantId = "000000" // 后端提供
 export const clientId = "1ee401e271db3f15cf61c93c5884bdd2" // 后端提供
 export const grantTypeXcx = "uniXcx" // 后台授权类型，授权方法
-export const platform = "mp-weixin" // 小程序平台标识，后台未使用
+export const platform = "weixin" // UniApp的provider平台标识，后台未使用
 export const appId = "wx0c6a0700e554d079" // 小程序ID
 export const domain = 'http://localhost:8080' // 后台接口域名
 // export const domain = 'https://www.domain.com/prod-api' // 后台接口域名
